37b958f8c5d4b595ce698635c94d2fe29e53b9c2
fatemzh <abid.fatem@hotmail.fr>
2023-09-03 13:37:31 +0200

    Forms backend DONE

--- frontend/src/page/SignUp.js
+++ frontend/src/page/SignUp.js
@@ -49,11 +49,24 @@ function SignUp() {
     })
   };
 
-  const handleSubmit = (e) => {
+console.log(process.env.REACT_APP_SERVER_DOMAIN)
+
+  const handleSubmit = async(e) => {
     e.preventDefault();
     const { firstName, lastName, email, password, confirmPassword } = data;
     if (firstName && lastName && email && password && confirmPassword) {
       if (password === confirmPassword) {
+        const fetchData = await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}signup`,{
+          method : 'POST',
+          headers : {
+            "content-type" : "application/json"
+          },
+          body : JSON.stringify(data)
+        })
+
+        const dataRes = await fetchData.json()
+        console.log(dataRes)
+
         alert("successful");
         navigate("/login");
       } else {
@@ -66,20 +79,18 @@ function SignUp() {
 
   return (
     <div className="p-3 md:p-4">
-      <div className="w-full max-w-sm bg-white m-auto flex flex-col p-2">
-        {/* <h1 className='text-center text-2xl font-bold'>Sign up</h1> */}
-        <div className="w-20 overflow-hidden rounded-full drop-shadow-md shadow-md m-auto">
+      <div className="w-full max-w-sm bg-white m-auto flex flex-col p-4">
+        <div className="w-20 h-30 overflow-hidden rounded-full drop-shadow-md shadow-md m-auto relative">
           <img src={data.image ? data.image : loginSignUpImage} className="w-full" />
 
           <label htmlFor="profileImage">
-            <div className="absolute bottom-o h-1/3 bg-slate-500 w-full text-center cursor-pointer">
-              <p className="text-sm p-1 text-white">Upload</p>
+            <div className="absolute bottom-o h-1/3 bg-slate-500 bg-opacity-50 w-full text-center cursor-pointer">
+              <p className="text-sm p-1 text-white">Upload</p> 
             </div>
             <input
               type={"file"}
               id="profileImage"
               accept="image/*"
-              className="hidden"
               onChange={handleUploadProfileImage}
             />
           </label>
